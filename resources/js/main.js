1368abeb3f89f91bbe120886e5a6153630163614
Cezary Grochowina <cezary.grochowina@gmail.com>
2019-02-18 11:03:46 +0100

    Refactored to ES6+ standard TodoList-JS main.js file

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,77 +1,79 @@
-document.getElementById('add').addEventListener('click', addItemTodo);
-document.getElementById('item').addEventListener('keydown', enterPressed);
-
-function enterPressed(event) {
-  var key = event.keyCode;
+enterPressed = event => {
+  let key = event.keyCode;
   if (key === 13) {
     addItemTodo();
   }
-}
+};
 
-function removeTask(e) {
-  var parent = e.path[3];
-  var taskCard = e.path[2];
+removeTask = event => {
+  let parent = event.path[3];
+  let taskCard = event.path[2];
   parent.removeChild(taskCard);
-}
+};
 
-function moveToDone(e) {
-  var doneButton = e.path[0];
-  var buttonParent = e.path[1];
-  var taskCard = e.path[2];
-  var taskCardParent = e.path[3];
+moveToDone = event => {
+  let doneButton = event.path[0];
+  let buttonParent = event.path[1];
+  let taskCard = event.path[2];
+  let taskCardParent = event.path[3];
 
-  var targer = taskCardParent.id === 'todo' ? document.getElementById('done') : document.getElementById('todo');
+  let target = taskCardParent.id === 'todo' ? document.getElementById('done') : document.getElementById('todo');
 
   buttonParent.removeChild(doneButton);
   taskCardParent.removeChild(taskCard);
-  targer.appendChild(taskCard);
-}
+  target.appendChild(taskCard);
+};
 
-function addItemTodo() {
-  var input = document.getElementById('item');
-  var text = input.value;
-  if (text) {
-    //Create main element div named item (task card)
-    var item = document.createElement('div');
-    item.classList.add('container');
-    item.classList.add('taskCard');
+getText = () => {
+  let inputValue = document.getElementById('item').value;
+  return inputValue;
+};
 
-    //Create container for task text part
-    var textBlock = document.createElement('div');
-    textBlock.innerText = text;
-    textBlock.classList.add('textBlock');
+addItemTodo = () => {
+  let text = getText();
 
-    //Create container for buttons: removeTaskButton and taskDoneButton
-    var buttons = document.createElement('div');
-    buttons.classList.add('container');
-
-    //Create removeTaskButton
-    var remove = document.createElement('button');
-    remove.classList.add('removeButton');
-    remove.innerText = 'remove';
-    remove.addEventListener('click', removeTask);
-
-    //Create taskDoneButton
-    var done = document.createElement('button');
-    done.classList.add('doneButton');
-    done.innerText = 'done';
-    done.addEventListener('click', moveToDone);
-
-    //Add buttons into container
-    buttons.appendChild(remove);
-    buttons.appendChild(done);
-
-    //Add text part into task card
-    item.appendChild(textBlock);
-
-    //Add container with buttons into task card
-    item.appendChild(buttons);
-
-    //Add task card to lits of tasks to do
-    document.getElementById('todo').appendChild(item);
-    input.value = '';
-  } else {
+  if (!text) {
     //If text value is empty show alert box and do not add empty task
     alert('Nie podano zadania!');
+    return;
   }
-}
+
+  //Creating components
+  let item = document.createElement('div');
+  let textBlock = document.createElement('div');
+  let buttons = document.createElement('div');
+  let remove = document.createElement('button');
+  let done = document.createElement('button');
+
+  //Adding classes, values and behaviour
+  item.classList.add('container');
+  item.classList.add('taskCard');
+
+  textBlock.innerText = text;
+  textBlock.classList.add('textBlock');
+
+  buttons.classList.add('container');
+
+  remove.classList.add('removeButton');
+  remove.innerText = 'remove';
+  remove.addEventListener('click', removeTask);
+
+  done.classList.add('doneButton');
+  done.innerText = 'done';
+  done.addEventListener('click', moveToDone);
+
+  //Components combine
+  buttons.appendChild(remove);
+  buttons.appendChild(done);
+
+  item.appendChild(textBlock);
+  item.appendChild(buttons);
+
+  //Push new item
+  document.getElementById('todo').appendChild(item);
+  //Clear input value
+  document.getElementById('item').value = '';
+};
+
+document.getElementById('add').addEventListener('click', addItemTodo);
+document.getElementById('item').addEventListener('keydown', enterPressed);
